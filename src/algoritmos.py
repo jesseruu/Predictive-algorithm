b33f72f8a069ba59774a97578097eef2a31adc7d
Jesser Lemus <ijesser@outlook.com>
2021-05-04 19:30:02 -0500

    Add algoritmos.py

---
+++ src/algoritmos.py
@@ -0,0 +1,19 @@
+class algoritmos_supervisados():
+
+    def logistic_regression(dataset):
+        return 0
+
+    def nearest_neighbor(dataset):
+        return 0
+
+    def support_vector_machine(dataset):
+        return 0
+
+    def naive_bayes(dataset):
+        return 0
+
+    def decision_tree(dataset):
+        return 0
+
+    def random_forest_classification(dataset):
+        return 0
